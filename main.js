13a502fcbd2c6a8016e7a7cdc4999afe3ce64330
LazyMagician <ananthuqw@gmail.com>
2022-03-12 15:01:12 +0530

    new changes

--- main.js
+++ main.js
@@ -6,15 +6,23 @@ const addTask = () =>{
        var task_val = document.getElementById("task-input").value;
     //    console.log(task_val);
        task.innerHTML = `<div class="content">
-       <input type="text"  placeholder="${task_val}" readonly>
+       <input type="text" class=tasks placeholder="${task_val}" readonly>
        </div>
     <div class="action">
-    <button onClick="del(this)">Delete</button> </div>`;
+    <button class="edit" onClick="edit(this)">Edit</button> 
+    <button class="delete" onClick="del(this)">Delete</button> </div>`;
     // console.log(task);   
     // console.log(document.getElementById('task-input-1'));
     add_task.appendChild(task); 
     
 }
 const del = (e) =>{
+    console.log(e.parentNode.parentNode.parentNode);
     e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
+}
+const edit = (e) =>{ 
+    // task_in = document.getElementsByClassName("edit")
+    e.parentNode.previousElementSibling.childNodes[1].removeAttribute("readonly");
+    
+    // if (edit.innText)
 }
